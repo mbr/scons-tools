a2431226563892f6f36b92516358fcb117b4ee51
Marc Brinkmann <git@marcbrinkmann.de>
2010-07-24 21:34:36 +0200

    Added automatic fill-in table for frequencies and allowed specification of AVR_MMCU.

--- avrtools.py
+++ avrtools.py
@@ -3,17 +3,28 @@
 
 from SCons.Script import *
 
+cpu_freq_table = {
+	'atmega328p': 16*10**6,
+}
+
 def generate(env):
 	bld = Builder(action = 'avr-objcopy -O ihex -R .eeprom $SOURCE $TARGET',
 					 suffix = '.hex',
 					 src_suffix = '.elf')
 	env.Append(BUILDERS = {'AVRHex': bld})
 
-	archflags = Split('-mmcu=atmega328p -DF_CPU=16000000L')
+	archflags = Split('-mmcu=$AVR_MMCU -DF_CPU=$AVR_F_CPU' % env)
 	env.Replace(CC = 'avr-gcc',
 	            CXX = 'avr-g++',
 	            AR = 'avr-ar',
 	            RANLIB = 'avr-ranlib')
+	env.SetDefault(AVR_MMCU = 'atmega328p')
+
+	# freq table fillin
+	if env['AVR_MMCU'] not in cpu_freq_table and not 'F_CPU' in env:
+		print 'Warning: Unknown AVR_MMCU "%s" and AVR_F_CPU not set.' % env['AVR_MMCU']
+	env.SetDefault(AVR_F_CPU = cpu_freq_table.get(env['AVR_MMCU'], ''))
+
 	env.Append(CFLAGS = archflags,
 			   CXXFLAGS = archflags,
 			   LINKFLAGS = archflags + Split('--gc-sections'))
