52cf7c6952290a413cc2b82135b54496e0a7d57c
Marc Brinkmann <git@marcbrinkmann.de>
2012-03-01 12:19:25 +0100

    Added web builder.

---
+++ web.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf8
+
+"""Builders for common generated web files"""
+
+import re
+
+from SCons.Script import *
+
+# note: this is far from accurate or correct,
+#       but should handle 99% of the common cases
+import_re = re.compile('@import\s+' +
+                       '(?:url\()?' +
+                       """["']([^'"]*)["']""" +
+                       '\s*;')
+
+
+def less_scan(node, env, path):
+    return env.File(import_re.findall(node.get_text_contents()))
+
+
+def generate(env):
+    bld = Builder(action='lessc $SOURCE $TARGET',
+                  suffix='.css',
+                  src_suffix='.less')
+
+    s = Scanner(function=less_scan, skeys=['.less'])
+    env.Append(BUILDERS={'less': bld}, SCANNERS=s)
+
+
+def exists(env):
+    return env.Detect('lessc')
