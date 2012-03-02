3d166a910da03ef6725be4bfc8abf0d917f4b5ae
Marc Brinkmann <git@marcbrinkmann.de>
2012-03-02 13:40:06 +0100

    Added documents tools.

---
+++ documents.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from SCons.Script import *
+
+# 150 dpi, a4 (210mm x 297mm)
+a4_dim_px = (1240, 1754)
+
+def generate(env):
+    # converts a JPEG with the right aspect ration to an A4 PDF page
+    img_to_pdf = Builder(action = ' '.join(
+                        ['convert', '$SOURCE',
+                         '-units', 'PixelsPerInch',
+                         '-density', '150',
+                         '-quality', '80',
+                         '-resize', '%dx%d' % a4_dim_px,
+                         '$TARGET']))
+
+    # converts an SVG to PDF using inkscape
+    svg_to_pdf = Builder(action = ' '.join(
+                        ['inkscape',
+                         '--export-area-page',
+                         '--export-pdf=$TARGET',
+                         '$SOURCE']))
+
+    # concatenates PDF files using pdftk
+    pdf_merge = Builder(action = 'pdftk $SOURCES cat output $TARGET')
+
+    env.Append(BUILDERS={
+        'PDFMerge': pdf_merge,
+        'ImgToPDF': img_to_pdf,
+        'SVGToPDF': svg_to_pdf,
+    })
+
+
+def exists(env):
+    # we could detect if all tools are installed, however a user might want
+    # to use only some of them
+    return True
