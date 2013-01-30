b75f02088d7816cbd8ea4dad35cd7b1792f16f19
Marc Brinkmann <git@marcbrinkmann.de>
2013-01-30 01:46:57 +0100

    Added a dart2js builder/scanner.

--- web.py
+++ web.py
@@ -3,9 +3,20 @@
 
 """Builders for common generated web files"""
 
+import os
 import re
+from urlparse import urlparse
 
 from SCons.Script import *
+import SCons.Warnings
+
+
+class UnknownDependencyWarning(SCons.Warnings.Warning):
+    pass
+
+
+SCons.Warnings.enableWarningClass(UnknownDependencyWarning)
+
 
 # note: this is far from accurate or correct,
 #       but should handle 99% of the common cases
@@ -20,14 +31,51 @@ def less_scan(node, env, path):
     return env.File([node.get_dir().File(fn) for fn in imports])
 
 
-def generate(env):
-    bld = Builder(action='lessc $SOURCE $TARGET',
-                  suffix='.css',
-                  src_suffix='.less')
+def dart2js_scan(node, env, path):
+    deps_fn = str(node) + '.js.deps'
+    if os.path.exists(deps_fn):
+        dependencies = []
+        with open(deps_fn) as deps_file:
+            for line in deps_file:
+                o = urlparse(line.strip())
+                if not o.scheme == 'file':
+                    SCons.Warnings.warn(UnknownDependencyWarning,
+                                        'Cannot handle dependency "%s"' % line)
+                    continue
 
-    s = Scanner(function=less_scan, skeys=['.less'])
-    env.Append(BUILDERS={'less': bld}, SCANNERS=s)
+                dependencies.append(env.File(o.path))
+        return dependencies
+    return []
+
+
+def dart_emitter(target, source, env):
+    target.append(str(target[0]) + '.deps')
+    target.append(str(target[0]) + '.map')
+    return target, source
+
+
+dart2js_builder = Builder(action='dart2js -c $SOURCE -o$TARGET',
+                          suffix='.dart.js',
+                          src_suffix='.dart',
+                          emitter=dart_emitter)
+
+dart2js_scanner = Scanner(function=dart2js_scan,
+                          skeys=['.dart'])
+
+
+less_builder = Builder(action='lessc $SOURCE $TARGET',
+                       suffix='.css',
+                       src_suffix='.less')
+
+
+less_scanner = Scanner(function=less_scan, skeys=['.less'])
+
+
+def generate(env):
+    env.Append(BUILDERS={'less': less_builder,
+                         'Dart2Js': dart2js_builder},
+               SCANNERS=[less_scanner, dart2js_scanner])
 
 
 def exists(env):
-    return env.Detect('lessc')
+    return True
