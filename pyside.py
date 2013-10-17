90d357a3dd08b109bda7b0ef4e186af76c004362
Marc Brinkmann <git@marcbrinkmann.de>
2013-10-17 17:26:24 +0200

    Added probably questionable tool for merging UI files.

--- pyside.py
+++ pyside.py
@@ -5,9 +5,11 @@ def generate(env):
     env.Append(BUILDERS={
         'PySideUI': Builder(action='$PYSIDE_UIC $SOURCE > $TARGET',
                             src_suffix='.ui', suffix='.py',
-                            single_source=True)
+                            single_source=True),
+        'PySideUIMerge': Builder(action='$PYSIDE_UIMERGE $SOURCE > $TARGET')
     })
     env.SetDefault(PYSIDE_UIC='pyside-uic')
+    env.SetDefault(PYSIDE_UIMERGE='cat')
 
 
 def exists(env):
