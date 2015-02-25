1339e7838fa774a02cd726d8495e5df6b2e8d833
Marc Brinkmann <git@marcbrinkmann.de>
2015-02-25 14:37:48 +0100

    Support pyside-rcc.

--- pyside.py
+++ pyside.py
@@ -6,9 +6,13 @@ def generate(env):
         'PySideUI': Builder(action='$PYSIDE_UIC $SOURCE > $TARGET',
                             src_suffix='.ui', suffix='.py',
                             single_source=True),
-        'PySideUIMerge': Builder(action='$PYSIDE_UIMERGE $SOURCES > $TARGET')
+        'PySideUIMerge': Builder(action='$PYSIDE_UIMERGE $SOURCES > $TARGET'),
+        'PySideRC': Builder(action='$PYSIDE_RCC $SOURCE > $TARGET',
+                            src_suffix='.qrc', suffix='.py',
+                            single_source=True)
     })
     env.SetDefault(PYSIDE_UIC='pyside-uic')
+    env.SetDefault(PYSIDE_RCC='pyside-rcc')
     env.SetDefault(PYSIDE_UIMERGE='cat')
 
 
